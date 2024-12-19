1446818f4a68454371345f96c1394f824628d908
sameersdeshpande <87390339+sameersdeshpande@users.noreply.github.com>
2024-12-18 19:28:47 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.91%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -9,8 +9,9 @@ class Solution:
         # Traverse each character in the string
         for char in s:
             if char in bracket_map:  # If it's a closing bracket
-                top_element = stack.pop() if stack else '#'
-                if bracket_map[char] != top_element:  # Check for matching opening bracket
+                if stack and stack[-1] == bracket_map[char]:
+                    stack.pop()
+                else:
                     return False
             else:  # If it's an opening bracket
                 stack.append(char)
