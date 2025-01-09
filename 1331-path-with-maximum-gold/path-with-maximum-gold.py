5f6d36d10bef67ac190665ff73f05de622f497be
sameersdeshpande <87390339+sameersdeshpande@users.noreply.github.com>
2025-01-09 00:00:46 -0500

    Time: 4671 ms (12.41%) | Memory: 17.9 MB (22.78%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        ROWS , COLS = len(grid), len(grid[0])
+        def dfs(r,c):
+            if (min(r,c) <0 or r==ROWS or c==COLS or grid[r][c] ==0):
+                return 0
+            tmp = grid[r][c]
+            grid[r][c] = 0
+            res = 0
+            neighbors = [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]
+            for r2, c2 in neighbors:
+                res = max(res, tmp+dfs(r2,c2))
+            grid[r][c]=tmp
+            return res
+
+
+        res = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                res = max(res, dfs(r,c))
+        return res
+        
