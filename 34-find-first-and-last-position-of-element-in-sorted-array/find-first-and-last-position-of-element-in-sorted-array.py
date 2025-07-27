11ad7a999491eb54e3dcd3672b8ef93e02d8b7e8
sameersdeshpande <87390339+sameersdeshpande@users.noreply.github.com>
2025-07-27 01:46:22 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (20.46%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,23 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def findFirst(nums, target):
-            left, right = 0, len(nums) - 1
-            first = -1
-            while left <= right:
-                mid = (left + right) // 2
-                if nums[mid] == target:
-                    first = mid
-                    right = mid - 1  # keep searching on the left side
-                elif nums[mid] < target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            return first
+        def binarySearch(nums, target,findfirst):
+            left,right = 0, len(nums)-1
+            result = -1
 
-        def findLast(nums, target):
-            left, right = 0, len(nums) - 1
-            last = -1
             while left <= right:
-                mid = (left + right) // 2
+                mid = (left+right)//2
                 if nums[mid] == target:
-                    last = mid
-                    left = mid + 1  # keep searching on the right side
-                elif nums[mid] < target:
-                    left = mid + 1
+                    result = mid
+                    if findfirst:
+                        right = mid -1
+                    else:
+                        left = mid+1
+                elif nums[mid]<target:
+                    left = mid +1
                 else:
-                    right = mid - 1
-            return last
-
-        first = findFirst(nums, target)
-        last = findLast(nums, target)
+                    right = mid -1
+            return result
+        
+        first = binarySearch(nums,target, True)
+        last = binarySearch(nums, target, False)
         return [first, last]
